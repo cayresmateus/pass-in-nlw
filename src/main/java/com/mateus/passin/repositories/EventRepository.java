94aa4667e33193411920e70cbcb42922a2958319
mateus <mateuscayres3@gmail.com>
2024-04-03 13:14:58 -0400

    refactor: Just remove one 's'

--- src/main/java/com/mateus/passin/repositories/EventRepository.java
+++ src/main/java/com/mateus/passin/repositories/EventRepository.java
@@ -3,6 +3,6 @@ package com.mateus.passin.repositories;
 import com.mateus.passin.domain.event.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EventsRepository extends JpaRepository<Event, String> {
+public interface EventRepository extends JpaRepository<Event, String> {
 
 }
