f6f5905c97962e03d945376047789268b9b1ee9a
mateus <mateuscayres3@gmail.com>
2024-04-03 16:25:55 -0400

    feat: route /attendees/id

--- src/main/java/com/mateus/passin/controllers/EventController.java
+++ src/main/java/com/mateus/passin/controllers/EventController.java
@@ -1,8 +1,10 @@
 package com.mateus.passin.controllers;
 
+import com.mateus.passin.dto.attendee.AttendeesListResponseDTO;
 import com.mateus.passin.dto.event.EventIdDTO;
 import com.mateus.passin.dto.event.EventRequestDTO;
 import com.mateus.passin.dto.event.EventResponseDTO;
+import com.mateus.passin.services.AttendeeService;
 import com.mateus.passin.services.EventService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -13,19 +15,26 @@ import org.springframework.web.util.UriComponentsBuilder;
 @RequestMapping("/events")
 @RequiredArgsConstructor
 public class EventController {
-    private  final EventService service;
+    private  final EventService eventService;
+    private final AttendeeService attendeeService;
 
     @GetMapping("/{eventId}")
     public ResponseEntity<EventResponseDTO> getEvent(@PathVariable String eventId){
-        EventResponseDTO event = this.service.getEventDetail(eventId);
+        EventResponseDTO event = this.eventService.getEventDetail(eventId);
 
         return ResponseEntity.ok(event);
     }
 
     @PostMapping
     public ResponseEntity<EventIdDTO> createEvent(@RequestBody EventRequestDTO body, UriComponentsBuilder uriComponentsBuilder ){
-        EventIdDTO eventIdDTO = this.service.createEvent(body);
+        EventIdDTO eventIdDTO = this.eventService.createEvent(body);
         var uri = uriComponentsBuilder.path("/events/{eventId}").buildAndExpand(eventIdDTO.eventId()).toUri();
         return ResponseEntity.created(uri).body(eventIdDTO);
+    }
+
+    @GetMapping("/attendees/{eventId}")
+    public ResponseEntity<AttendeesListResponseDTO> getEventAttendees(@PathVariable String eventId){
+        AttendeesListResponseDTO attendeeListResponse = this.attendeeService.getEventAttendee(eventId);
+        return ResponseEntity.ok(attendeeListResponse);
     }
 }
