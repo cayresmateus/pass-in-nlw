398027ccd12d08650c50aa580cd260199ff2d3a2
mateus <mateuscayres3@gmail.com>
2024-04-03 13:14:04 -0400

    feat: Adding non existing method to count attendees amount

--- src/main/java/com/mateus/passin/repositories/AttendeeRepository.java
+++ src/main/java/com/mateus/passin/repositories/AttendeeRepository.java
@@ -3,6 +3,8 @@ package com.mateus.passin.repositories;
 import com.mateus.passin.domain.attendee.Attendee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AttendeeRepository extends JpaRepository<Attendee, String> {
+import java.util.List;
 
+public interface AttendeeRepository extends JpaRepository<Attendee, String> {
+    List<Attendee> findByEventId(String eventId);
 }
