18f3fefca21861602a1e0d2a914dfdfb74e207b7
mateus <mateuscayres3@gmail.com>
2024-04-04 08:35:51 -0400

    feat: methods to register participant

--- src/main/java/com/mateus/passin/controllers/EventController.java
+++ src/main/java/com/mateus/passin/controllers/EventController.java
@@ -1,5 +1,7 @@
 package com.mateus.passin.controllers;
 
+import com.mateus.passin.dto.attendee.AttendeeIdDTO;
+import com.mateus.passin.dto.attendee.AttendeeRequestDTO;
 import com.mateus.passin.dto.attendee.AttendeesListResponseDTO;
 import com.mateus.passin.dto.event.EventIdDTO;
 import com.mateus.passin.dto.event.EventRequestDTO;
@@ -30,6 +32,12 @@ public class EventController {
         EventIdDTO eventIdDTO = this.eventService.createEvent(body);
         var uri = uriComponentsBuilder.path("/events/{eventId}").buildAndExpand(eventIdDTO.eventId()).toUri();
         return ResponseEntity.created(uri).body(eventIdDTO);
+    }
+    @PostMapping("/{eventId}/attendees")
+    public ResponseEntity<AttendeeIdDTO> registerParticipant(@PathVariable String eventId, @RequestBody AttendeeRequestDTO body, UriComponentsBuilder uriComponentsBuilder ){
+        AttendeeIdDTO attendeeIdDTO = this.eventService.registerAttendeeOnEvent(eventId,body);
+        var uri = uriComponentsBuilder.path("/attendees/{attendees}").buildAndExpand(attendeeIdDTO.attendeeId()).toUri();
+        return ResponseEntity.created(uri).body(attendeeIdDTO);
     }
 
     @GetMapping("/attendees/{eventId}")
--- src/main/java/com/mateus/passin/services/EventService.java
+++ src/main/java/com/mateus/passin/services/EventService.java
@@ -2,7 +2,10 @@ package com.mateus.passin.services;
 
 import com.mateus.passin.domain.attendee.Attendee;
 import com.mateus.passin.domain.event.Event;
+import com.mateus.passin.domain.event.exceptions.EventFullException;
 import com.mateus.passin.domain.event.exceptions.EventNotFoundException;
+import com.mateus.passin.dto.attendee.AttendeeIdDTO;
+import com.mateus.passin.dto.attendee.AttendeeRequestDTO;
 import com.mateus.passin.dto.event.EventIdDTO;
 import com.mateus.passin.dto.event.EventRequestDTO;
 import com.mateus.passin.dto.event.EventResponseDTO;
@@ -11,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.text.Normalizer;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -19,7 +23,7 @@ public class EventService {
     private  final EventRepository eventRepository;
     private final AttendeeService attendeeService;
     public EventResponseDTO getEventDetail(String eventId){
-        Event event = this.eventRepository.findById(eventId).orElseThrow(() -> new EventNotFoundException("Event not found with id " + eventId));
+        Event event = this.getEventById(eventId);
         List<Attendee> attendeeList = this.attendeeService.getAllAttendeesFromEvent(eventId);
         return new EventResponseDTO(event, attendeeList.size());
     }
@@ -34,6 +38,27 @@ public class EventService {
         return new EventIdDTO(newEvent.getId());
 
     }
+    public AttendeeIdDTO registerAttendeeOnEvent(String eventId, AttendeeRequestDTO attendeeRequestDTO){
+        this.attendeeService.verifyAttendeeSubscription(attendeeRequestDTO.email(),eventId);
+        Event event = this.getEventById(eventId);
+        List<Attendee> attendeeList = this.attendeeService.getAllAttendeesFromEvent(eventId);
+
+        if(event.getMaximumAttendees() <= attendeeList.size()) throw new EventFullException("Event is full");
+
+        Attendee newAttendee = new Attendee();
+        newAttendee.setName(attendeeRequestDTO.name());
+        newAttendee.setEmail(attendeeRequestDTO.email());
+        newAttendee.setEvent(event);
+        newAttendee.setCreatedAt(LocalDateTime.now());
+        this.attendeeService.registerAttendee(newAttendee);
+
+        return new AttendeeIdDTO(newAttendee.getId());
+    }
+
+    private Event getEventById(String eventId){
+        return this.eventRepository.findById(eventId).orElseThrow(() -> new EventNotFoundException("Event not found with id " + eventId));
+    }
+
     private String createSlug(String text){
         String normalized = Normalizer.normalize(text, Normalizer.Form.NFD);
         return normalized.replaceAll("[\\p{InCOMBINING_DIACRITICAL_MARKS}]", "")
