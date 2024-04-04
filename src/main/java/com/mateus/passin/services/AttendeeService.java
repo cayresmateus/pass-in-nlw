a9973feed655524edf69def6e309916529791de5
mateus <mateuscayres3@gmail.com>
2024-04-04 08:39:11 -0400

    feat: services to register attendee on events and check in

--- src/main/java/com/mateus/passin/services/AttendeeService.java
+++ src/main/java/com/mateus/passin/services/AttendeeService.java
@@ -1,13 +1,18 @@
 package com.mateus.passin.services;
 
 import com.mateus.passin.domain.attendee.Attendee;
+import com.mateus.passin.domain.attendee.exceptions.AttendeeAlreadyExistsException;
+import com.mateus.passin.domain.attendee.exceptions.AttendeeNotFoundException;
 import com.mateus.passin.domain.checkIn.CheckIn;
+import com.mateus.passin.dto.attendee.AttendeeBadgeResponseDTO;
 import com.mateus.passin.dto.attendee.AttendeeDetails;
 import com.mateus.passin.dto.attendee.AttendeesListResponseDTO;
+import com.mateus.passin.dto.attendee.AttendeeBadgeDTO;
 import com.mateus.passin.repositories.AttendeeRepository;
 import com.mateus.passin.repositories.CheckInRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -17,7 +22,7 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class AttendeeService {
     private final AttendeeRepository attendeeRepository;
-    private final CheckInRepository checkInRepository;
+    private final CheckInService checkInService;
 
     public List<Attendee> getAllAttendeesFromEvent(String eventId){
         return this.attendeeRepository.findByEventId(eventId);
@@ -27,11 +32,36 @@ public class AttendeeService {
         List<Attendee> attendeeList = this.getAllAttendeesFromEvent(eventId);
 
         List<AttendeeDetails> attendeeDetailsList = attendeeList.stream().map(attendee -> {
-            Optional<CheckIn> checkIn = this.checkInRepository.findByAttendeeId(attendee.getId());
+            Optional<CheckIn> checkIn = this.checkInService.getCheckIn(attendee.getId());
             LocalDateTime checkedInAt = checkIn.<LocalDateTime>map(CheckIn::getCreatedAt).orElse(null);
             return new AttendeeDetails(attendee.getId(), attendee.getName(), attendee.getEmail(), attendee.getCreatedAt(), checkedInAt);
         }).toList();
         return new AttendeesListResponseDTO(attendeeDetailsList);
+    }
+    public void verifyAttendeeSubscription(String email, String eventId ){
+        Optional<Attendee> isAttendeeRegistered = this.attendeeRepository.findByEventIdAndEmail(eventId,email);
+        if(isAttendeeRegistered.isPresent()) throw new AttendeeAlreadyExistsException("Attendee is already registered");
+    }
+
+    public Attendee registerAttendee(Attendee newAttendee){
+        this.attendeeRepository.save(newAttendee);
+        return newAttendee;
+    }
+    public void checkInAttendee(String attendeeId){
+        Attendee attendee = this.getAttendee(attendeeId);
+        this.checkInService.registerCheckIn(attendee);
+    }
+
+    private Attendee getAttendee(String attendeeId){
+        return this.attendeeRepository.findById(attendeeId).orElseThrow(() -> new AttendeeNotFoundException("Attendee not found with id " + attendeeId));
+    }
+
+    public AttendeeBadgeResponseDTO getAttendeeBadge(String attendeeId, UriComponentsBuilder uriComponentsBuilder){
+        Attendee attendee = this.getAttendee(attendeeId);
+        var uri = uriComponentsBuilder.path("/attendees/{attendeesId}/check-in").buildAndExpand(attendeeId).toUri().toString();
+
+        AttendeeBadgeDTO attendeeBadgeDTO = new AttendeeBadgeDTO(attendee.getName(),attendee.getEmail(), uri, attendee.getEvent().getId());
+        return new AttendeeBadgeResponseDTO(attendeeBadgeDTO);
     }
 
 }
