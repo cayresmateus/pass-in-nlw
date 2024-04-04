c57f52d475b21d8a306a736698759de2adf8edd6
mateus <mateuscayres3@gmail.com>
2024-04-04 09:16:59 -0400

    feat: exception handling

--- src/main/java/com/mateus/passin/config/ExceptionEntityHandler.java
+++ src/main/java/com/mateus/passin/config/ExceptionEntityHandler.java
@@ -1,6 +1,12 @@
 package com.mateus.passin.config;
 
+import com.mateus.passin.domain.attendee.exceptions.AttendeeAlreadyExistsException;
+import com.mateus.passin.domain.attendee.exceptions.AttendeeNotFoundException;
+import com.mateus.passin.domain.checkIn.exception.CheckInAlreadyExistsException;
+import com.mateus.passin.domain.event.exceptions.EventFullException;
 import com.mateus.passin.domain.event.exceptions.EventNotFoundException;
+import com.mateus.passin.dto.general.ErrorResponseDTO;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,4 +17,22 @@ public class ExceptionEntityHandler {
     public ResponseEntity handleEventNotFound(){
         return ResponseEntity.notFound().build();
     }
+
+    @ExceptionHandler(EventFullException.class)
+    public ResponseEntity handleEventFull(EventFullException exception){
+        return ResponseEntity.badRequest().body(new ErrorResponseDTO(exception.getMessage()));
+    }
+    @ExceptionHandler(AttendeeNotFoundException.class)
+    public ResponseEntity handleAttendeeNotFound(AttendeeNotFoundException exception){
+        return ResponseEntity.notFound().build();
+    }
+    @ExceptionHandler(AttendeeAlreadyExistsException.class)
+    public ResponseEntity handleAttendeeAlreadyExists(AttendeeAlreadyExistsException exception){
+        return ResponseEntity.status(HttpStatus.CONFLICT).build();
+    }
+    @ExceptionHandler(CheckInAlreadyExistsException.class)
+    public ResponseEntity handleCheckInAlreadyExists(CheckInAlreadyExistsException exception){
+        return ResponseEntity.status(HttpStatus.CONFLICT).build();
+    }
+
 }
